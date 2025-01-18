f4a272c3329f0b29c70959fb1ec4b8bf54b4dc09
jdmartens <jeremydmartens@gmail.com>
2025-01-17 20:50:09 -0600

    add oauth

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
 from flask_sqlalchemy import SQLAlchemy
 from config import settings
 
@@ -7,6 +8,18 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(settings)
+
+    # app.secret_key = os.urandom(24)  # Use a secure random key in production
+    oauth = OAuth(app)
+
+    oauth.register(
+        name='oidc',
+        authority='https://cognito-idp.us-east-2.amazonaws.com/us-east-2_uxevrdBQe',
+        client_id='5t0a625as4n1dhpp5vfdbb9aqr',
+        client_secret='<client secret>',
+        server_metadata_url='https://cognito-idp.us-east-2.amazonaws.com/us-east-2_uxevrdBQe/.well-known/openid-configuration',
+        client_kwargs={'scope': 'phone openid email'}
+    )
 
     db.init_app(app)
 
