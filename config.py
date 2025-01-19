49ca76a43dfc5cda1c0ca73bff06bc9db5dbd60a
jdmartens <jeremydmartens@gmail.com>
2025-01-18 20:14:41 -0600

    additional cognito params

--- config.py
+++ config.py
@@ -10,7 +10,12 @@ class Config(BaseSettings):
     AWS_REGION: str = 'us-east-1'
     S3_BUCKET: str
     COGNITO_USER_POOL_ID: str
+    
     COGNITO_APP_CLIENT_ID: str
+    COGNITO_APP_CLIENT_SECRET: str
+    COGNITO_AUTHORITY: str
+    COGNITO_META_URL: str
+
     FLASK_RUN_PORT: int
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
