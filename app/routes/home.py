cfa6d6e7ca631840c701c312a379245a1998cd36
jdmartens <jeremydmartens@gmail.com>
2025-01-18 19:06:20 -0600

    home route bp

---
+++ app/routes/home.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, session
+
+bp = Blueprint('home', __name__)
+
+@bp.route('/')
+def index():
+    user = session.get('user')
+    if user:
+        return f'Hello, {user["email"]}. <a href="/logout">Logout</a>'
+    else:
+        return f'Welcome! Please <a href="/login">Login</a>.'
