83b54a764e56342a518925b5102d6e8c6355723c
jdmartens <jeremydmartens@gmail.com>
2025-01-18 20:15:38 -0600

    add logout

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, session, redirect, url_for
 import boto3
-from app import oauth
+from app import oauth, db
 import logging
 
 bp = Blueprint('auth', __name__)
@@ -19,7 +19,7 @@ def login():
     # Alternate option to redirect to /authorize
     # redirect_uri = url_for('authorize', _external=True)
     # return oauth.oidc.authorize_redirect(redirect_uri)
-    return oauth.oidc.authorize_redirect('http://localhost:5001')
+    return oauth.oidc.authorize_redirect('http://localhost:5001/authorize')
 
 @bp.route('/authorize')
 def authorize():
@@ -29,11 +29,16 @@ def authorize():
     session['user'] = user
     return redirect(url_for('index'))
 
+@bp.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('user.index'))
 
-# @bp.route('/init_db')
-# def init_db():
-#     db.create_all()
-#     return jsonify({"message": "Database initialized"}), 200
+
+@bp.route('/init_db')
+def init_db():
+    db.create_all()
+    return jsonify({"message": "Database initialized"}), 200
 
 
 
