6ee54a8bef1de06affddbaf1fac7cda5e84f2ca3
jdmartens <jeremydmartens@gmail.com>
2025-01-18 20:30:10 -0600

    new user_form

---
+++ app/forms/user_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, FileField
+from wtforms.validators import DataRequired, Email
+
+class UserForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    admin = BooleanField('Admin')
+    profile_pic = FileField('Profile Picture')
