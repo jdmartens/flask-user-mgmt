570d709c64f7e49a0549253b81387fe0e728a44a
jdmartens <jeremydmartens@gmail.com>
2025-01-19 19:31:56 -0600

    configure app to use migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask, app, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import settings
 
 db = SQLAlchemy()
 oauth = OAuth()
+login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -20,10 +24,12 @@ def create_app():
         client_id=settings.COGNITO_APP_CLIENT_ID,
         client_secret=settings.COGNITO_APP_CLIENT_SECRET,
         server_metadata_url=settings.COGNITO_META_URL,
-        client_kwargs={'scope': 'phone openid email'}
+        client_kwargs={'scope': 'openid email profile'}
     )
 
     db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes import auth, user, group, home
     app.register_blueprint(auth.bp)
@@ -32,3 +38,9 @@ def create_app():
     app.register_blueprint(home.bp)
 
     return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models.user import User # Avoid circular import
+    return User.query.get(int(user_id))
