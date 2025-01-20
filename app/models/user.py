d8019baeebbdaf5726a87f0eab6574564356c933
jdmartens <jeremydmartens@gmail.com>
2025-01-19 19:32:45 -0600

    add fields to support flask current_user

--- app/models/user.py
+++ app/models/user.py
@@ -9,5 +9,23 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     profile_pic = db.Column(db.String(255))
     last_signed_on = db.Column(db.DateTime, default=datetime.utcnow)
-    admin = db.Column(db.Boolean, default=False)
+    role = db.Column(db.String(50), default='user')
+
+    def get_id(self):
+        return str(self.id)
+
+    @property
+    def is_active(self):
+        # Return True if the user is active and allowed to log in
+        return True
+
+    @property
+    def is_authenticated(self):
+        # Return True if the user is authenticated
+        return True
+
+    @property
+    def is_anonymous(self):
+        # Return False as we do not support anonymous users
+        return False
 
