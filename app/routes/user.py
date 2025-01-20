79c1ac28fa02fca2f144e4b8e2e585591ec4d584
jdmartens <jeremydmartens@gmail.com>
2025-01-19 20:31:34 -0600

    set creds on client create

--- app/routes/user.py
+++ app/routes/user.py
@@ -74,8 +74,11 @@ def edit_user(user_id):
         form.populate_obj(user)
         if form.profile_pic.data:
             filename = secure_filename(form.profile_pic.data.filename)
-            s3 = boto3.client('s3')
-            s3.upload_fileobj(form.profile_pic.data, Config.S3_BUCKET, filename)
+            s3 = boto3.client('s3',
+                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                      region_name=settings.AWS_REGION)
+            s3.upload_fileobj(form.profile_pic.data, settings.S3_BUCKET, filename)
             user.profile_pic = filename
         db.session.commit()
         flash('User updated successfully', 'success')
