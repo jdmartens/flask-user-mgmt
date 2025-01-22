ca7aa192318c7c8c7c962227d41d3483dc448d2b
jdmartens <jeremydmartens@gmail.com>
2025-01-21 22:30:20 -0600

    populate all group choices

--- app/forms/user_form.py
+++ app/forms/user_form.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, FileField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, Email
 
+from app.models.group import Group
+
 class UserForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
@@ -9,3 +11,8 @@ class UserForm(FlaskForm):
     profile_pic = FileField('Profile Picture')
     role = SelectField('Role', choices=[('user', 'User'), ('admin', 'Admin')], validators=[DataRequired()])
     groups = SelectMultipleField('Groups', coerce=int)
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.role.choices = [('user', 'User'), ('admin', 'Admin')]
+        self.groups.choices = [(group.id, group.name) for group in Group.query.all()]
